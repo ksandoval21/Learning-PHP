40589ccb12dd1de4079bd7ca88ff77aa8a2eb4b0
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-02-03 14:24:10 -0600

    minute 34 of learning PHP video FCC

---
+++ site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<h1>Ken's Site</h1>";
+    echo "<hr>";
+    echo "<p>This is my first time using PHP<p>"
+?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $charname = "Kenia";
+    $charage = 18;
+    echo "<br>My name is $charname.</br>";
+    echo "<br>I am $charage years old.</br>";
+
+?>
+</body>
+</html>
